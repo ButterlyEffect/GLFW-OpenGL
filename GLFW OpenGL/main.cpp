7054d596df732ed6db7303a0cc241ca7f2d000dd
ButterlyEffect <zorrilla4163@gmail.com>
2024-06-04 11:30:20 -0400

    Initial Commit

---
+++ GLFW OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GLFW OpenGL
+//
+//  Created by Simon Zorrilla on 6/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
